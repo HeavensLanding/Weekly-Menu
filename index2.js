6f2e75bfbc8dcf421aafd07a538a40f0a47d6fac
HeavensLanding <rasmussencourtney0103@gmail.com>
2024-07-23 20:43:16 -0600

    table changeover

---
+++ index2.js
@@ -0,0 +1,27 @@
+let id = 0;
+
+document.getElementById('add').addEventListener('click',() => {
+    let table = document.getElementById('tables-container');
+    let row = table.insertRow(1);
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById('dayOfWeek').value;
+    row.insertCell(1).innerHTML = document.getElementById('mealType').value;
+    row.insertCell(2).innerHTML = document.getElementById('new-entree').value;
+    row.insertCell(3).innerHTML = document.getElementById('new-sides').value;
+    let actions = row.insertCell(4)
+    actions.appendChild(createDeleteButton(id++));
+    document.getElementById('dayOfWeek').value = '';
+});
+
+function createDeleteButton(id) {
+    let btn = document.createElement('button');
+    btn.className = 'btn btn-primary';
+    btn.id = id;
+    btn.innerHTML = 'Delete';
+    btn.onclick = () => {
+        console.log(`Deleting row with id: item-${id}`)
+        let elementToDelete = document.getElementById(`item-${id}`)
+        elementToDelete.parentNode.removeChild(elementToDelete);
+    };
+    return btn;
+}
